54adfdb7d1556e2a504c4769f3fd358ab301b7a0
Javier Enrique <s0wkete@gmail.com>
2014-11-06 12:58:38 -0300

    scaffolding of the site

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
+// Init foundation
 $(document).foundation();
+
+// Calculate width of google map
+window.onload = function() {
+    $("#map_content").css("width", $(window).width());
+    // $('#collection-home  .hover').fadeIn();
+};
+$( window ).resize(function() {
+    $("#map_content").css("width", $(window).width());
+});
