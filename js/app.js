c9418c02060a00104accbad0b090b18da3b29964
Javier Enrique <s0wkete@gmail.com>
2014-11-06 18:50:04 -0300

    responsive google maps

--- js/app.js
+++ js/app.js
@@ -4,8 +4,26 @@ $(document).foundation();
 // Calculate width of google map
 window.onload = function() {
     $("#map_content").css("width", $(window).width());
+    $("#map_content").css("height", $(".over_map").height()+150);
     // $('#collection-home  .hover').fadeIn();
 };
 $( window ).resize(function() {
     $("#map_content").css("width", $(window).width());
+    $("#map_content").css("height", $(".over_map").height()+150);
+});
+
+// Smooth scroll
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
